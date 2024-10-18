f5067a7a1c52e5106cac0b62145dd0eae1e7fff4
Rootul Patel <rootulp@gmail.com>
2024-10-18 15:17:49 -0400

    feat: diverge from upstream main
    fix: disable asdf because I do not use it
    fix: logs of the form compinit:527: no such file or directory: /opt/homebrew/share/zsh/site-functions/_brew_services
    chore: ignore VSCode
    chore: add Cargo env
    docs: add README

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -2,11 +2,11 @@
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
 # Try loading ASDF from the regular home dir location
-if [ -f "$HOME/.asdf/asdf.sh" ]; then
-  . "$HOME/.asdf/asdf.sh"
-elif which brew >/dev/null && [ -f "$(brew --prefix asdf)/libexec/asdf.sh" ]; then
-  . "$(brew --prefix asdf)/libexec/asdf.sh"
-fi
+# if [ -f "$HOME/.asdf/asdf.sh" ]; then
+#   . "$HOME/.asdf/asdf.sh"
+# elif which brew >/dev/null; then
+#   . "$(brew --prefix asdf)/libexec/asdf.sh"
+# fi
 
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
